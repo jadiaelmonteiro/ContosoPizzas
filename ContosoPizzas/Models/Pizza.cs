4201986f1395c72ba82e884638a4627632279036
Jadiael Monteiro <jadiael.monteiro@gmail.com>
2022-01-23 10:46:37 -0300

    Adding local data storage

---
+++ ContosoPizzas/Models/Pizza.cs
@@ -0,0 +1,9 @@
+namespace ContosoPizzas.Models
+{
+    public class Pizza
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public bool IsGlutenFree { get; set; }
+    }
+}
